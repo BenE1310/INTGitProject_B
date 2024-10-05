02e9c1ef1756279fe6e26236f85a66f8cc40592a
bene1310 <beneytan5@gmail.com>
2024-10-05 09:28:15 +0300

    change app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 
 @app.route("/status")
-def home():
+def status():
     return "OK"
 
 
@@ -22,26 +22,24 @@ def blog():
 def pricing():
     return "Our pricing for web development starts at $70 per hour."
 
-
 @app.route("/contact")
 def contact():
     return "You can contact us at contact@example.com or info@example.com."
 
 
 @app.route("/chat")
-def contact():
+def chat():
     return "You can chat with live agent!"
+
 
 @app.route("/services")
 def services():
     return "We offer the following services: web development, data analysis, SEO."
 
+
 @app.route("/internal")
-def home():
+def internal():
     return "System management portal"
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port=22, host="0.0.0.0")
-
-
+    app.run(debug=True, port=8081, host="0.0.0.0")
